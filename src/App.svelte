akx/illuminatrix

commit a7eb03fbf32849feda929acb60368a9ceddbe0d3

Persist API object

src/App.svelte
@@ -15,17 +15,16 @@
   } from "./consts";
   import { delay, getColorRGBs } from "./helpers";
   import * as apply from "./apply";
-  import type { LightAPI } from "./api/base";
-  import EmbeddedHALightAPI from "./api/embedded-ha";
-  import ExternalHALightAPI from "./api/external-ha";
   import cx from "clsx";
   import type { Palette } from "./palettes/types";
   import type { Hass } from "./types/ha";
   import type { LightState, SetLight } from "./types/app";
   import { getColors, getEnabledLights, persistState } from "./state";
+  import { getAPI } from "./api/helpers";
 
-  export let hass: Hass | null = null;
+  export const hass: Hass | null = null;
   const darkMode = !!hass?.themes.darkMode;
+  const lightAPI = getAPI(hass);
 
   let enabledLights: string[] = getEnabledLights();
   let colors: string[] = getColors();
@@ -37,15 +36,8 @@
   let cVariation: Variation = { ...defaultCVariation };
   let hVariation: Variation = { ...defaultHVariation };
 
-  function getAPI(): LightAPI {
-    if (hass?.states && hass?.callService) {
-      return new EmbeddedHALightAPI(hass);
-    }
-    return new ExternalHALightAPI();
-  }
-
   async function reloadLights() {
-    lights = await getAPI().getLightStates();
+    lights = await lightAPI.getLightStates();
   }
 
   async function applyColors(variation: boolean) {
@@ -57,7 +49,7 @@
       // probably out of quota
     }
     const result = await apply.applyColors({
-      api: getAPI(),
+      api: lightAPI,
       rgbs,
       entityIds: enabledLights,
       lVariation: variation ? lVariation : null,
@@ -84,15 +76,14 @@
 
   async function setLight(e: CustomEvent<SetLight>) {
     const { entityId, brightness, color, state } = e.detail;
-    const api = getAPI();
     if (brightness !== undefined) {
-      await api.setLightBrightness(entityId, brightness);
+      await lightAPI.setLightBrightness(entityId, brightness);
     }
     if (color !== undefined) {
-      await api.setLightColor(entityId, color);
+      await lightAPI.setLightColor(entityId, color);
     }
     if (state !== undefined) {
-      await api.setLightState(entityId, state);
+      await lightAPI.setLightState(entityId, state);
     }
     await delay(500);
     await reloadLights();

src/api/helpers.ts
@@ -1,0 +1,11 @@
+import type { Hass } from "../types/ha";
+import type { LightAPI } from "./base";
+import EmbeddedHALightAPI from "./embedded-ha";
+import ExternalHALightAPI from "./external-ha";
+
+export function getAPI(hass: Hass | null): LightAPI {
+  if (hass?.states && hass?.callService) {
+    return new EmbeddedHALightAPI(hass);
+  }
+  return new ExternalHALightAPI();
+}

commit 88356a690621979dc9221a802a05da0c86841622

Fix embedded API not updating light state
Regressed in a7eb03f

src/App.svelte
@@ -20,11 +20,19 @@
   import type { Hass } from "./types/ha";
   import type { LightState, SetLight } from "./types/app";
   import { getColors, getEnabledLights, persistState } from "./state";
-  import { getAPI, isValidHass } from "./api/helpers";
+  import { isValidHass } from "./api/helpers";
+  import EmbeddedHALightAPI from "./api/embedded-ha";
+  import ExternalHALightAPI from "./api/external-ha";
 
   export let hass: Hass | null = null;
+  const getHass = () => {
+    if (isValidHass(hass)) return hass;
+    throw new Error("Invalid hass");
+  };
   const darkMode = isValidHass(hass) ? hass.themes.darkMode : false;
-  const lightAPI = getAPI(hass);
+  const lightAPI = isValidHass(hass)
+    ? new EmbeddedHALightAPI(getHass)
+    : new ExternalHALightAPI();
 
   let enabledLights: string[] = getEnabledLights();
   let colors: string[] = getColors();

src/api/embedded-ha.ts
@@ -6,11 +6,17 @@ import {
 import type { Hass } from "../types/ha";
 
 export default class EmbeddedHALightAPI extends BaseHALightAPI {
-  private hass: Hass;
+  private _getHass: () => Hass;
 
-  constructor(hass: Hass) {
+  // We need a trampoline function to get the newest `hass` object
+  // that the HA UI passes down to our panel.
+  constructor(getHass: () => Hass) {
     super();
-    this.hass = hass;
+    this._getHass = getHass;
+  }
+
+  private get hass() {
+    return this._getHass();
   }
 
   callLightService(service: string, payload: Record<string, any>) {

src/api/helpers.ts
@@ -1,15 +1,5 @@
 import type { Hass } from "../types/ha";
-import type { LightAPI } from "./base";
-import EmbeddedHALightAPI from "./embedded-ha";
-import ExternalHALightAPI from "./external-ha";
 
 export function isValidHass(hass: any): hass is Hass {
   return hass?.states && hass?.callService;
-}
-
-export function getAPI(hass: Hass | null): LightAPI {
-  if (isValidHass(hass)) {
-    return new EmbeddedHALightAPI(hass);
-  }
-  return new ExternalHALightAPI();
 }